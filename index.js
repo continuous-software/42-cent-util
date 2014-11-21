e2b0130924a4c7b1247e7ad6ef7dbfd722a737d6
lorenzofox3 <laurent34azerty@gmail.com>
2014-11-21 17:14:58 +0700

    add JSDoc

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+/**
+ * copy object values to another object with a key mapping based on a schema.
+ * A new Object is created if target is not specified
+ * If value is already on the target object for a given key, it will be overwritten.
+ * Only keys in the schema will be mapped.
+ * @example
+ *
+ * var schema = {
+ *  prop: '_prop',
+ *  otherProp: '_otherProp',
+ *  mayExist: '_mayExist'
+ * };
+ *
+ * var object = {
+ *  prop: 'value',
+ *  otherProp: 'otherValue',
+ *  notInSchema: 'error'
+ * };
+ *
+ * var output = mapKeys(object, schema);
+ *
+ * assert.equal(output._prop, 'value');
+ * assert.equal(output._otherProp, 'otherValue');
+ * assert(output.notInSchema === undefined);
+ * assert(output.prop === undefined);
+ * assert(output.otherProp === undefined);
+ * assert(output.mayExist === undefined);
+ * assert(output._mayExist === undefined);
+ *
+ * @param {Object} source - the source object
+ * @param {Object} schema - the schema to apply
+ * @param {Object} [target] - a target object
+ * @returns {Object} the output object
+ */
 function mapKeys(source, schema, target) {
 
     var obj = target || {};
